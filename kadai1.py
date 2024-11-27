b3fa7effca55a081be147f88b7327543ec6bbfc3
Satoshi Nemoto <s23c1104tl@s.chibakoudai.jp>
2024-11-27 10:29:03 +0900

    課題

--- kadai1.py
+++ kadai1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # SPDX-FileCopyrightText: 2024 Satoshi Nemoto miraiprj3104@icloud.com
+# SPDX-License-Identifier: BSD-3-Clause
+
 import sys
 
 money = (int(52) * int(sys.argv[2]))
