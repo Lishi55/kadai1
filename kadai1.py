13ee9a563863f979681ef36f4a1a966c5650911f
Satoshi Nemoto <s23c1104tl@s.chibakoudai.jp>
2024-11-28 13:24:23 +0900

    kadai

--- kadai1.py
+++ kadai1.py
@@ -3,8 +3,13 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import sys
-
-money = (int(52) * float(sys.argv[2]))
-money = (money * int(sys.argv[1]))
-money = (money/float(10000))
-print("アルバイト年収は{}万円です".format(money))
+if int(sys.argv[1]) >= 1076:
+    money = (int(52) * float(sys.argv[2]))
+    money = (money * int(sys.argv[1]))
+    money = (money/float(10000))
+    if 103 <= money:
+        print("103万の壁突破!!!")
+    print("アルバイト年収は{}万円です".format(money))
+else:
+    print("千葉県の最低賃金を下回っています")
+    print("労基に相談しましょう")
--- test.bash
+++ test.bash
@@ -8,7 +8,18 @@ ng () {
 res=0
 
 out=$(./kadai1.py 1200 20)
-[ "${out}" = "アルバイト年収は124.8万円です" ] || ng "$LINENO"
+expected="103万の壁突破!!!
+アルバイト年収は124.8万円です"
+[ "${out}" = "${expected}" ] || ng "$LINENO"
+
+out=$(./kadai1.py 1100 10)
+expected="アルバイト年収は57.2万円です"
+[ "${out}" = "${expected}" ] || ng "$LINENO"
+
+out=$(./kadai1.py 1000 10)
+expected="千葉県の最低賃金を下回っています
+労基に相談しましょう"
+[ "${out}" = "${expected}" ] || ng "$LINENO"
 
 out=$(./kadai1.py 1200)
 [ "$?" = 1 ] || ng "$LINENO"
